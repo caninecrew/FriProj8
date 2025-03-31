f6b82fe11e12329e8e509a962dd0d47816016a79
caninecrew <samuelleerumbley@gmail.com>
2025-03-30 23:21:28 -0500

    Add user interaction for contact information form

---
+++ friProj8.py
@@ -0,0 +1,12 @@
+# User interaction (start with text input)
+
+# name, birthday, email, phone number, address, and a dropdown menu for preferred contact method
+
+print("Contact Information Form")
+print("Please fill out the following information:")
+name = input("Name: ")
+birthday = input("Birthday (YYYY-MM-DD): ")
+email = input("Email: ")
+phone_number = input("Phone Number: ")
+address = input("Address: ")
+preferredContact = input("Preferred Contact Method (Email/Phone): ")
