8781a60c24b82b43ca4dc4a2714ae9a85dce79f7
caninecrew <samuelleerumbley@gmail.com>
2025-03-31 02:05:01 -0500

    Add button widget to main window in gui.py

--- gui.py
+++ gui.py
@@ -1,4 +1,7 @@
 from tkinter import * # Import the tkinter module
 from tkinter import ttk # Import the ttk module for themed widgets
 root = Tk() # Create the main window
+
+button1 = ttk.Button(root).grid()
+
 root.mainloop() # Start the main event loop
