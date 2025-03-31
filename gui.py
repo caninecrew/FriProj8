a6e17641449f85193619901329f9ce6196f86235
caninecrew <samuelleerumbley@gmail.com>
2025-03-31 02:09:15 -0500

    Add text to submit button in gui.py

--- gui.py
+++ gui.py
@@ -2,6 +2,8 @@ from tkinter import * # Import the tkinter module
 from tkinter import ttk # Import the ttk module for themed widgets
 root = Tk() # Create the main window
 
-button1 = ttk.Button(root).grid()
+button1 = ttk.Button(root, text="Submit")
+button1.grid()
+
 
 root.mainloop() # Start the main event loop
