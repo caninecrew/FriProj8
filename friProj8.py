47e42b0ecdfdffd8a93a80a2c0375d8636edfbbc
caninecrew <samuelleerumbley@gmail.com>
2025-03-31 00:33:46 -0500

    Refactor contact information input into a separate collectInfo function for better organization

--- friProj8.py
+++ friProj8.py
@@ -20,19 +20,19 @@ CREATE TABLE IF NOT EXISTS customers (
 # Commit the changes to the database
 conn.commit()
 
-print("Contact Information Form")
-print("Please fill out the following information:")
-name = input("Name: ")
-birthday = input("Birthday (MM-DD-YYYY): ")
-email = input("Email: ")
-phone_number = input("Phone Number: ")
-address = input("Address: ")
-city = input("City: ")
-state = input("State/Province: ")
-zip = input("ZIP/Postal Code: ")
-country = input("Country: ")
-preferredContact = input("Preferred Contact Method (Email/Phone): ")
-
+def collectInfo():
+    print("Contact Information Form")
+    print("Please fill out the following information:")
+    name = input("Name: ")
+    birthday = input("Birthday (MM-DD-YYYY): ")
+    email = input("Email: ")
+    phone_number = input("Phone Number: ")
+    address = input("Address: ")
+    city = input("City: ")
+    state = input("State/Province: ")
+    zip = input("ZIP/Postal Code: ")
+    country = input("Country: ")
+    preferredContact = input("Preferred Contact Method (Email/Phone): ")
 
 class Customer: # This class represents a customer and their contact information
     def __init__(self, name, birthday, email, phone_number, address, city, state, zip, country, preferredContact):
