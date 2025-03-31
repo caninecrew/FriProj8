a41c414a53fcdd4a6c4f1a1effc4299ebb59db48
caninecrew <samuelleerumbley@gmail.com>
2025-03-31 02:09:28 -0500

    Add customer and database imports in gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,8 @@
 from tkinter import * # Import the tkinter module
 from tkinter import ttk # Import the ttk module for themed widgets
+from customer import Customer
+from database import view_all_customers
+
 root = Tk() # Create the main window
 
 button1 = ttk.Button(root, text="Submit")
